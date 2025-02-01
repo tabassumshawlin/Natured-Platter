947847c8ff4b7116783285394b64da5b28bf613a
tabassumshawlin <tabassumshawlin65@gmail.com>
2025-02-01 23:52:48 +0600

    dids

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          marquee: 'marquee 10s linear infinite',
+        },
+        keyframes: {
+          marquee: {
+            '0%': { transform: 'translateX(100%)' },
+            '100%': { transform: 'translateX(-100%)' },
+          },
+        },
+      },
     },
-    plugins: [],
-  }
+  };
+  
